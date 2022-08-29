4059f65d79168f938db03da38ea66b62be8673c0
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-08-29 19:00:09 +0200

    audio: add -y option to dnf

--- audio.sh
+++ audio.sh
@@ -1,3 +1,3 @@
-sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+sudo dnf install -y http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
 http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install mplayer ffmpeg vlc
+sudo dnf install -y mplayer ffmpeg vlc
