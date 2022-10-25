f50121214798f30d16f7e600b31b688fbd4017d2
gucio321 <gucio321@protonmail.com>
2022-10-25 08:33:35 +0200

    bashcfg: update day_to_the_end_of_hear.sh

--- resources/bashcfg/day_to_the_end_of_year.sh
+++ resources/bashcfg/day_to_the_end_of_year.sh
@@ -1,11 +1,28 @@
-#how meny days to the end of year
-days=`echo "366-\`date +%j\`" |bc`
+#!/bin/bash
+
+daysPerYear="365"
+eyDate="2023-04-30"
+eyYearDay=`date --date=$eyDate +%j`
+cYearDay=`date +%j`
+cYear=`date +%Y`
+eyYear=`date --date=$eyDate +%Y`
+result="0"
+
+# jeśli jesteśmy już w tym samym roku
+if [ "$cYear" == "$eyYear" ]; then
+	result=`echo "$eyYearDay-$cYearDay" |bc`
+elif [ "$cYear" -lt "$eyYear" ]; then
+	result=`echo "$daysPerYear-$cYearDay+$eyYearDay" |bc`
+else
+	result="Err"
+fi
+
 function stars() {
-if [ "$days" -gt "100" ]; then
+if [ "$result" -gt "100" ]; then
   for i in `seq 36`; do
     echo -n "*"
   done
-elif [ "$days" -lt "100" ] && [ $days -gt "9" ]; then
+elif [ "$result" -lt "100" ] && [ $result -gt "9" ]; then
   for i in `seq 35`; do
     echo -n "*"
   done
@@ -17,6 +34,6 @@ fi
 }
 
 stars
-echo -e "\n* pozostalo $days dni do konca roku! *"
+echo -e "\n* pozostalo $result dni do konca roku! *"
 stars
 echo -ne "\n"
